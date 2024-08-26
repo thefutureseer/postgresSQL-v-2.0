2e9bd3254862df73f54ba6a8a6b2c14bf53826a5
thefutureseer <danielharrislimos@outlook.com>
2024-08-26 04:31:06 -0700

    show user id name and email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
-// import { sql } from "@vercel/postgres";
+import { sql } from "@vercel/postgres";
 
-// export default async function HomePage() {
-//   const { rows } = await sql`SELECT * FROM car_rides`;
+export default async function Cart({
+  params
+} : {
+  params: { user: string }
+}): Promise<JSX.Element> {
+  const { rows } = await sql`SELECT * from "user"`;
 
-//   return (
-//     <div>
-//       <h1>Car Rides</h1>
-//       {rows.length > 0 ? (
-//         rows.map((row) => (
-//           <div key={row.id}>
-//             <p>ID: {row.id}</p>
-//             <p>Quantity: {row.quantity}</p>
-//           </div>
-//         ))
-//       ) : (
-//         <p>No car rides found.</p>
-//       )}
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      {rows.map((row) => (
+        <div key={row.id}>
+          {row.id} 
+          <p>Name: {row.name}</p>
+          <p>Email: {row.email}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
